8a40a89d37917ff3430ddf7ce5cea096aebac6c0
TaiyabaBhatti <taiyababhattii@gmail.com>
2024-11-08 22:11:29 +0500

    added textForm

--- React-Project-1/src/App.jsx
+++ React-Project-1/src/App.jsx
@@ -4,12 +4,14 @@ import './index.css'
 // import TabsLink  from './headerTabs'
 import Navbar from './CustomComponents/Navbar.jsx'
 import Hero from './CustomComponents/Hero.jsx'
+import Counter from './CustomComponents/Counter.jsx'
 
 function App() {
  return (
 <>
 <Navbar/>
 <Hero/>
+{/* <Counter/> */}
 </>
  )
 }
