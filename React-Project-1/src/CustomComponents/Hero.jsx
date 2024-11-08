1c2a08adbc66ecb029c7e9bbecced35d842b3d17
TaiyabaBhatti <taiyababhattii@gmail.com>
2024-11-08 13:08:25 +0500

    Arrays, Arrays Rendering and Conditionals

--- React-Project-1/src/CustomComponents/Hero.jsx
+++ React-Project-1/src/CustomComponents/Hero.jsx
@@ -3,9 +3,13 @@ import Cards from "./Cards"
 function Hero(){
 return (<>
 <section className="mt-10 px-5 m-auto max-w-7xl flex flex-row gap-3 justify-center">
-<Cards title="History" desc="some day  can be sold out"/>
-<Cards title="Relevant Words" desc="Wish list canot be filled with expectations"/>
-<Cards title="Errors" desc="Never ever restore the bugs"/>
+
+<Cards title="History" desc="some day  can be sold out" btnText="Read more" link="https://www.pinterest.com" listArr={[1,2,3]} price={3000}/>
+
+<Cards title="Relevant Words" desc="Wish list canot be filled with expectations" btnText="Go beyond" link="https://www.unsplash.com" listArr={["a","b","c"]} price={1400}/>
+
+<Cards title="Errors" desc="Never ever restore the bugs" btnText="More Around" link="https://www.freepik.com" listArr={["A","B","C"]} price={800}/>
+
 </section>
 </>)
 }
