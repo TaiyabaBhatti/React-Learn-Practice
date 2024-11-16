a32b0fdc5e3b33662cd033e96dad56d64a5feeef
TaiyabaBhatti <taiyababhattii@gmail.com>
2024-11-17 04:35:32 +0500

    modified

--- React-Project-1/src/App.jsx
+++ React-Project-1/src/App.jsx
@@ -5,12 +5,14 @@ import './index.css'
 import Navbar from './CustomComponents/Navbar.jsx'
 import Hero from './CustomComponents/Hero.jsx'
 import Counter from './CustomComponents/Counter.jsx'
+import Counter2 from './CustomComponents/Counter2.jsx'
 
 function App() {
  return (
 <>
 <Navbar/>
-<Hero/>
+{/* <Hero/> */}
+<Counter2/>
 {/* <Counter/> */}
 </>
  )
