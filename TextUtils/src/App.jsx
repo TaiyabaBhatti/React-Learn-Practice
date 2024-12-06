072fe0e21d5a87b7da3b678e758de1acb71bc06e
TaiyabaBhatti <taiyababhattii@gmail.com>
2024-12-06 21:47:02 +0500

    creating alert dialogue component

--- TextUtils/src/App.jsx
+++ TextUtils/src/App.jsx
@@ -4,11 +4,27 @@ import About from './About'
 import Hero from './Hero'
 import './index.css'
 import Navbar from './Navbar'
+import AlertDialogue from './AlertDialogue'
 
 function App() {
 
 const [mode,setMode] =useState(true);
 const [text,setText] =useState("Light");
+const [showComponent,setShowComponent] =useState(true);
+
+const onDelete = () =>{
+
+if(true){
+setShowComponent(false);
+}
+
+else{
+    setShowComponent(true);
+}
+}
+
+
+
 
 const toggleMode = () => {
 
@@ -16,20 +32,21 @@ if(mode){
 // light mode is alreday enabled, now switch to dark
 setMode(false);
 setText("Dark")
+document.body.style.backgroundColor="#828385";
 }
 
 else{
     setMode(true);
     setText("Light")
+    document.body.style.backgroundColor="white";
 }
-
-
 }
 
 
 
  return (
 <>
+<AlertDialogue show={onDelete}/>
 <Navbar mode={mode}  toggle={toggleMode} switchText= {text}/>
 <Hero mode={mode}/>
 {/* <About/> */}
