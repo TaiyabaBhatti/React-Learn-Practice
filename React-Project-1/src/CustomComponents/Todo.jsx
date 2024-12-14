3a8ee689dec91fb3456dae24fdb76f58fd043bad
TaiyabaBhatti <taiyababhattii@gmail.com>
2024-12-14 12:04:04 +0500

    update the overall items and specif items by iteratings its unique id, used map to iterate and create a copy of array of objects

--- React-Project-1/src/CustomComponents/Todo.jsx
+++ React-Project-1/src/CustomComponents/Todo.jsx
@@ -1,54 +1,91 @@
-import React, { useState } from 'react'
-import TodoDisplay from './TodoDisplay';
-import {v4 as uuidv4} from 'uuid'
+import React, { useState } from "react";
+import TodoDisplay from "./TodoDisplay";
+import { v4 as uuidv4 } from "uuid";
 
 export default function Todo() {
+  const [newItem, setNewItem] = useState("");
+  const [todosArr, setArr] = useState([]);
 
+  const addItem = () => {
+    if (newItem === "") {
+      return;
+    }
 
-    const [newItem,setNewItem] = useState("");
-    const [todosArr,setArr] = useState([]);
-const addItem = () => {
+    setArr((prev) => {
+      return [...prev, { task: newItem, key: uuidv4() }];
+    });
 
-if(newItem === ""){
-    return;
-}
-
-setArr((prev)=>{
-    return [...prev,{task:newItem, key:uuidv4() }]
-})
-
- setNewItem("")
-
-}
-
-const handleInput = (event) =>{
-
+    setNewItem("");
+  };
+  const handleInput = (event) => {
     let inputValue = event.target.value;
     setNewItem(inputValue);
+  };
+  // delteting by creting copy of array then filtering those elements we only want
+  const deletItem = (unId) => {
+    setArr((prev) => {
+      return [...prev.filter((item) => item.key != unId)];
+    });
+  };
 
-}
+  const updateAllListItems = () => {
+    setArr(
+      todosArr.map((item) => {
+        return { ...item, task: item.task.toUpperCase() };
+      })
+    );
+  };
 
-const deletItem = (unId) => {
+  const updateSpecific = (id) => {
+   setArr((preArr) => {
+    return preArr.map((item) => {
 
-    setArr((prev)=>{
-        return [...prev.filter(item => item.key != unId)]
+        if(item.key === id){
+            return {...item,task:item.task.toUpperCase()}
+        }
+
+        else{
+            return item
+        }
+
     })
-  
-    
-    }
-    
+   }
 
 
+
+  )
+
+  };
 
   return (
-    <div className='m-auto py-2 px-3 max-w-80 mt-6 bg-gray-400'>
-<div className='flex gap-x-2 place-content-center'>
-    <input type="text" value={newItem} onChange={handleInput} className='outline-none px-2'/>
-    <button className='rounded-md
-     bg-blue-600 px-5 py-1 text-white active:scale-105 transition-all duration-200' onClick={addItem}>Add</button>
-</div>
-<TodoDisplay listItems={todosArr} remove={deletItem}/>
-
+    <div className="m-auto py-2 px-3 w-fit mt-6 bg-gray-400">
+      <div className="flex gap-x-2 place-content-center">
+        <input
+          type="text"
+          value={newItem}
+          onChange={handleInput}
+          className="outline-none px-2"
+        />
+        <button
+          className="rounded-md
+     bg-blue-600 px-5 py-1 text-white active:scale-105 transition-all duration-200"
+          onClick={addItem}
+        >
+          Add
+        </button>
+        <button
+          className="rounded-md
+     bg-blue-600 px-5 py-1 text-white active:scale-105 transition-all duration-200"
+          onClick={updateAllListItems}
+        >
+          UpdateAll
+        </button>
+      </div>
+      <TodoDisplay
+        listItems={todosArr}
+        remove={deletItem}
+        update={updateSpecific}
+      />
     </div>
-  )
+  );
 }
--- React-Project-1/src/CustomComponents/TodoDisplay.jsx
+++ React-Project-1/src/CustomComponents/TodoDisplay.jsx
@@ -2,11 +2,6 @@ import React from 'react'
 
 
 export default function TodoDisplay(props) {
-
-
-
-
-
   return (
     props.listItems && 
    <ul className='ml-5 mt-5 flex flex-col gap-y-3'>
@@ -18,6 +13,11 @@ export default function TodoDisplay(props) {
          <span>{items.task}</span>
          <button className='rounded-md
      bg-blue-600 px-5 py-1 text-white active:scale-105 transition-all duration-200' onClick={()=>{props.remove(items.key)}}>Rem</button>
+     <button className="rounded-md
+     bg-blue-600 px-5 py-1 text-white active:scale-105 transition-all duration-200" onClick={()=>{props.update(items.key)}}
+        >
+          Update
+        </button>
          </li>
 })}
    </ul>
