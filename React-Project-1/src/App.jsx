1cb303c11bdb46c05445b63b9b2aa2f2e23526ae
TaiyabaBhatti <taiyababhattii@gmail.com>
2024-12-15 02:02:48 +0500

    basic workin cleared

--- React-Project-1/src/App.jsx
+++ React-Project-1/src/App.jsx
@@ -1,11 +1,10 @@
-import FormCom from './CustomComponents/FormCom'
 import './index.css'
 import { useEffect, useState } from 'react'
 
 export default function App() {
  return (
 <>
-<FormCom/>
+
 </>
  )
 }
